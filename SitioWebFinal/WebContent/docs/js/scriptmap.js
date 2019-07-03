ee210374a0786ec9ff29d48f0101b71b93e86d83
misterpremium <simbacat@hotmail.es>
2019-07-03 11:46:35 +0200

    Revert "Update SQLpref.jar"
    This reverts commit ba1c1a7155840aa56739a07715aa34ba50022f74.

--- SitioWebFinal/WebContent/docs/js/scriptmap.js
+++
@@ -1,9 +0,0 @@
-
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(40.4674649, -3.4580456),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-}
