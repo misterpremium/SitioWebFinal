05f3e2f750386a51cf8e5f059355b74ea1ea3396
misterpremium <simbacat@hotmail.es>
2019-08-14 13:33:29 +0200

    test

---
+++ src/servlet/ConnectionJMX.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConnectionJMX
+ */
+@WebServlet("/ConnectionJMX")
+public class ConnectionJMX extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+   /* public ConnectionJMX() {
+        super();
+        // TODO Auto-generated constructor stub
+    }*/
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//test
+		
+		
+		ProcessBuilder builder = new ProcessBuilder("java", "-jar", "AMQ.jar");		
+		Process proces = builder.start();
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + proces + "</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+}
