74f1e6dbe5912093ddae8a29aee1c22ad59abbf8
misterpremium <simbacat@hotmail.es>
2019-08-16 13:02:41 +0200

    Modified AMQ.jar and ConectionJMX.java

--- src/servlet/ConnectionJMX.java
+++ src/servlet/ConnectionJMX.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.*;  
+
+import amqlib.*;
+
+
+
 
 /**
  * Servlet implementation class ConnectionJMX
@@ -31,15 +38,56 @@ public class ConnectionJMX extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
+		
+		String saludo = "hola", despedida = "adios";
 		//test v4
+		//Producctor.enviaMensajeCola("Hola");
+		//Main test = new Main();
+	
+		//test.Main.ejecutarCola("Hola", "Adios");
+		
+		//this.ejecutarCola(saludo, despedida);
+		
+		
+		Class<Main> c=Main.class;  
+		Object obj = null;
+		try {
+			obj = c.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		  
+		Method m = null;
+		try {
+			m = c.getDeclaredMethod("ejecutarCola",new Class[]{String.class,String.class});
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		m.setAccessible(true);
+		try {
+			m.invoke(obj,saludo, despedida);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
 		
 		ProcessBuilder builder = new ProcessBuilder("java", "-jar", "AMQ.jar");		
 		Process proces = builder.start();
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<h1>" + proces + "</h1>");
+		
+		
+		
 	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
